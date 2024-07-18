e8591662b4fcd91f1bbb3c14d29f7e07f428bd30
sbe06079 <144533350+sbe06079@users.noreply.github.com>
2024-07-17 22:56:44 -0400

    Delete lib directory

--- lib/components/text_size.dart
+++
@@ -1,2 +0,0 @@
-// Define a variable textSize and set its value to 16
-const double standardTextSize = 16.0;
--- lib/components/website_colors.dart
+++
@@ -1,8 +0,0 @@
-// constants.dart
-import 'package:flutter/material.dart';
-
-class WebsiteColors {
-  static const Color accentBlue = Color(0xFF9FDAFB);
-  static const Color hyperlinkPurple = Color(0xFF50C4AE);
-  //static const Color hyperlinkPurple = Color(0xFFD8D9AA);
-}
